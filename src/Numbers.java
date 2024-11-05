f35e25e9d0a3c60e245edb24f5da449e9aef09d0
Vasu-2001 <tirumalavasu2001@gmail.com>
2024-11-05 11:12:05 -0500

    sucuess

--- src/Numbers.java
+++
@@ -1,9 +0,0 @@
-
-public class Numbers {
-
-	public static void main(String[] args) {
-	System.out.println("welcome to numbers");
-
-	}
-
-}
---
+++ src/com/java/arrays/Num.java
@@ -0,0 +1,11 @@
+package com.java.arrays;
+
+public class Num {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome to java");
+
+	}
+
+}
