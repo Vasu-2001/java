1b25ff6785835b0b739c9f6893a6ee5b88000071
Vasu-2001 <tirumalavasu2001@gmail.com>
2024-11-08 00:30:05 -0500

    sucess

---
+++ src/com/java/collections/Test.java
@@ -0,0 +1,22 @@
+package com.java.collections;
+
+import java.util.ArrayList;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+           ArrayList al = new ArrayList();
+           al.add(1);
+           al.add(2);
+           al.add(3);
+           al.add(4);
+           System.out.println(al);
+           al.remove(3);
+           System.out.println(al);
+             
+           
+           
+	}
+
+}
